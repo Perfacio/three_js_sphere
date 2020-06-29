f9722602de822ecb247cc5778e29570490b0a2e8
Татьяна Перфильева <tanya@foothold.ru>
2020-06-29 17:49:30 +0300

    threejs

---
+++ html/js/script.js
@@ -0,0 +1,55 @@
+window.onload = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var canvas = document.getElementById('canvas');
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+    var ball = {
+        positionX: 0,
+        positionY: 0,
+        positionZ: 0,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0
+    };
+
+    var gui = new dat.GUI();
+    gui.add(ball, 'rotationY' ).min(-0.2).max(0.2).step(0.001)
+
+    var renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setClearColor(0x000000);
+
+    var scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 5000);
+    camera.position.set(0, 0, 1000);
+
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+
+
+    var geometry = new THREE.SphereGeometry(200, 12, 12);
+
+    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors});
+
+    for(var i=0; i < geometry.faces.length; i++ ){
+        geometry.faces[i].color.setRGB(Math.random(), Math.random(), Math.random());
+    }
+
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    function loop(){
+        mesh.rotation.y += ball.rotationY;
+        renderer.render(scene, camera);
+        requestAnimationFrame(function(){loop();});
+
+    }
+    loop();
+
+
+
+
+
+}
